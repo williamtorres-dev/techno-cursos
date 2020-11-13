c5135717cd877e92c60c399953277f83322275b2
William Tôrres <williammacairatorres@gmail.com>
2020-11-13 11:31:55 -0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contato from '../views/Contato.vue'
+import Cursos from '../views/Cursos.vue'
+import Curso from '../views/Curso.vue'
+import Aula from '../views/Aula.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/contato",
+    name: "contato",
+    component: Contato,
+  },
+  {
+    path: "/cursos",
+    name: "cursos",
+    component: Cursos,
+  },
+  {
+    path: "/cursos/:curso",
+    name: "curso",
+    component: Curso,
+    props: true,
+    children: [
+      {
+        path: ":aula",
+        name: "aula",
+        component: Aula,
+        props: true,
+      }
+    ]
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="loading">
+      <PageLoading/>
+    </div>
+    <transition>
+      <div v-if="api" class="conteudo">
+        <div>
+          <h1>Sobre a {{api.titulo}}</h1>
+          <p>{{api.descricao}}</p>
+          <router-link class="btn-cursos" tag="button" to="/cursos">Cursos</router-link>
+          <div>
+            <h2 class="avaliacoes">Avaliações</h2>
+            <ul>
+              <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                <p class="nome">{{avaliacao.nome}}</p>
+                <p class="descricao">{{avaliacao.descricao}}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src="@/assets/aprender.png" alt="Aprenda Web Design">
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import fetchData from "@/mixins/fetchData.js";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "home",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/home");
   }
-}
+};
 </script>
+
+<style>
+.btn-cursos {
+  border: none;
+  background: #4b8;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  padding: 15px 40px;
+  font-size: 1rem;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  box-shadow: 0 4px 2px rgba(0, 0, 0, 0.1);
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+
+h2.avaliacoes {
+  margin-bottom: 40px;
+}
+
+ul li p.nome {
+  font-weight: bold;
+}
+
+ul li p.descricao {
+  margin-bottom: 40px;
+}
+
+</style>
